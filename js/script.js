ce53c37cb941f757b84e74bd5a8f6588b99b3d5c
Jenny <jenniferonsamer@hotmail.com>
2017-10-22 20:59:27 -0700

    Achieved to only append 12 stories

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // Built by LucyBot. www.lucybot.com
 
-$('#select-article').on('change', function(){
+$('#select-article').on('change', function () {
 
   $('#content').empty();
 
   var selectVal = $('#select-article').val();
 
-  var url = "https://api.nytimes.com/svc/topstories/v2/" + selectVal + ".json";
+  var url = 'https://api.nytimes.com/svc/topstories/v2/' + selectVal + '.json';
   url += '?' + $.param({
-    'api-key': "80315f98187c49169c887403d2c3f766"
+    'api-key': '80315f98187c49169c887403d2c3f766'
   });
   $.ajax({
     url: url,
@@ -17,12 +17,45 @@ $('#select-article').on('change', function(){
 
     console.log(data.results);
 
-    $.each(data.results, function(index, object){
-      console.log(object.title);
+    // var sum = 0;
 
-      $('#content').append(object.title);
+    $.each(data.results, function( index, value ) {
+      
+      // sum += value;
+
+    $('#content').filter( ':odd' ).css( 'background-color', 'red' );
+          // console.log(value.title);
+          // $('#content').slice()
+
+          $('#content').append('<li>' + value.title + '</li>');
+          if (index === 11) {
+            return false;
+          }
+
 
     });
+   
+
+    // var resultCount = data.results.length;
+    // console.log(resultCount);
+
+
+    //   for (var i = 0; i < resultCount; i++){
+    //       console.log('test');
+    //     // total += data.results[i].title;
+    //   }
+
+    // $('#content').append('<li>' + data.results[total].title + '</li>')
+
+    // $.each(data.results, function(index, object){
+    //   console.log(object);
+
+    //   // var output = '';
+    //   output += '<h1>' + data.title + '</h1>';
+
+    //   $('#content').append(output);
+
+    // });
 
     // console.log(data.results[0].title);
 
@@ -32,5 +65,5 @@ $('#select-article').on('change', function(){
 
   });
 
-  
+
 });
